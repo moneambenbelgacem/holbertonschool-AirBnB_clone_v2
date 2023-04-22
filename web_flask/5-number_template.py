810a6d0f23f4a9492bdbfed4f06343f895a76b05
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-04-22 13:13:19 +0100

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -48,7 +48,7 @@ def number(n):
 @app.route("/number_template/<int:n>")
 def number_template(n):
     """Number template router"""
-    return render_template("templates/5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
