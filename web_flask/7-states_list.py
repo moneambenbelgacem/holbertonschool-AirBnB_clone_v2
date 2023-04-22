f1e07428d28b633b2098ed7a49cfe3c0df9b6055
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-04-22 16:31:22 +0100

    render state template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,8 +7,10 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
 
 @app.route('/')
 def hello_hbnb():
@@ -21,5 +23,5 @@ def diplay():
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+    
